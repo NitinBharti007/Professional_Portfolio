e01cda6d4226bb0a34d962503b9e6e8f8df826db
Nitin Bharti <jacksp198@gmail.com>
2025-09-15 00:11:59 +0530

    Error fixed

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,20 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
